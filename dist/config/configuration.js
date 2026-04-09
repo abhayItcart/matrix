f0b9554b9eab9895f79919c73b73cd44f707a94a
Abhay Singh <abhay.s@itcart.io>
2026-04-09 21:54:09 +0530

    Deleted package-lock.json

--- dist/config/configuration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({
-    port: parseInt(process.env.PORT || '3000', 10),
-    identityServiceUrl: process.env.IDENTITY_SERVICE_URL || 'http://localhost:3000',
-    faceAiServiceUrl: process.env.FACE_AI_SERVICE_URL || 'https://face-recognition.aixhub.ai',
-});
-//# sourceMappingURL=configuration.js.map
